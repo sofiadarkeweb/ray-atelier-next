7912f9cb41243742951993c64cbb89e49300c0b6
Sofia Darke <sofia.darke@tradera.com>
2026-04-17 18:18:13 +0200

    Center name in navbar

--- components/NavBar.js
+++ components/NavBar.js
@@ -5,15 +5,21 @@ const NavBar = () => {
   return (
     <>
       <nav className="nav-bar" id="foo">
-        <Link href="/projects" className="nav-bar-items">
-          Work
-        </Link>
-        <Link href="/about" className="nav-bar-items">
-          Ray Atelier
-        </Link>
-        <Link href="/contact" className="nav-bar-items">
-          Contact
-        </Link>
+        <div className="nav-bar-side nav-bar-left">
+          <Link href="/projects" className="nav-bar-items">
+            Work
+          </Link>
+        </div>
+        <div className="nav-bar-center">
+          <Link href="/about" className="nav-bar-items">
+            Ray Atelier
+          </Link>
+        </div>
+        <div className="nav-bar-side nav-bar-right">
+          <Link href="/contact" className="nav-bar-items">
+            Contact
+          </Link>
+        </div>
       </nav>
     </>
   );
